128aadfabae4536e6c382ef8436006bd4ec7085f
Bharath Kalkote K V <150000982+bharath-kalkote23@users.noreply.github.com>
2025-06-05 20:25:07 +0530

    Update heatmap&pairplot.py

--- heatmap&pairplot.py
+++ heatmap&pairplot.py
@@ -1,15 +1,4 @@
-import numpy as np , pandas as pd 
-import matplotlib.pyplot as plt , seaborn as sns
+import pandas as pd, seaborn as sns 
 from sklearn.datasets import fetch_california_housing
 df = fetch_california_housing(as_frame=True).frame
-plt.figure(figsize=(10,15))
-for i,feature in enumerate(df.columns):
-  q1,q3 = df[feature].quantile(0.25),df[feature].quantile(0.75)
-  lb,ub = q1-1.5*(q3-q1),q3+1.5*(q3-q1)
-  print(f'{feature},{len(df[(df[feature]<lb) | (df[feature]>ub)])}')
-  plt.subplot(6,3,i+1)
-  sns.histplot(df[feature],color='g')
-  plt.subplot(6,3,i+10)
-  sns.boxplot(x=df[feature],color='r')
-plt.tight_layout()
-print(df.describe())
+sns.heatmap(df.corr(),annot=True);sns.pairplot(df)
